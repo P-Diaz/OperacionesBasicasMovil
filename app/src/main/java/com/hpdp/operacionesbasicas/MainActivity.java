7f7bcfd6386fddcd76f0ec4b58ade2b1280c028b
P-Diaz <132587300+P-Diaz@users.noreply.github.com>
2023-11-15 10:46:53 -0500

    creacion de actividad para operacion suma

--- app/src/main/java/com/hpdp/operacionesbasicas/MainActivity.java
+++ app/src/main/java/com/hpdp/operacionesbasicas/MainActivity.java
@@ -3,12 +3,26 @@ package com.hpdp.operacionesbasicas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText edNumero1, edNumero2;
+    TextView tvResultado;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        edNumero1 = findViewById(R.id.txtNumero1);
+        edNumero2 = findViewById(R.id.txtNumero2);
+        tvResultado = findViewById(R.id.lblResultado);
+    }
+
+    public void CalcularSuma(View v){
+        double num1 = Double.parseDouble(edNumero1.getText().toString());
+        int num2 = Integer.parseInt(edNumero2.getText().toString());
+        double resultado = num1 + num2;
+        tvResultado.setText(resultado+"");
     }
 }
