66ae1db3af31b07f4afac92e22d5f396116e3f77
P-Diaz <132587300+P-Diaz@users.noreply.github.com>
2023-11-16 00:10:17 -0500

    Conexion del frontend con el backend

--- app/src/main/java/com/hpdp/operacionesbasicas/Menu.java
+++ app/src/main/java/com/hpdp/operacionesbasicas/Menu.java
@@ -3,12 +3,17 @@ package com.hpdp.operacionesbasicas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class Menu extends AppCompatActivity {
-
+    Button suma, resta, division, multi;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        suma = findViewById(R.id.btnSuma);
+        resta = findViewById(R.id.btnResta);
+        division = findViewById(R.id.btnDivision);
+        multi = findViewById(R.id.btnMulti);
     }
 }
