31738c705396fb088fa8f004932f789bbce72f39
P-Diaz <132587300+P-Diaz@users.noreply.github.com>
2023-11-16 21:21:46 -0500

    Creacion de la logica de la multiplicacion

--- app/src/main/java/com/hpdp/operacionesbasicas/Multiplicacion.java
+++ app/src/main/java/com/hpdp/operacionesbasicas/Multiplicacion.java
@@ -3,12 +3,26 @@ package com.hpdp.operacionesbasicas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Multiplicacion extends AppCompatActivity {
 
+    EditText edNumero1, edNumero2;
+    TextView tvResultado;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_multiplicacion);
+        edNumero1 = findViewById(R.id.txtNumeroMu1);
+        edNumero2 = findViewById(R.id.txtNumeroMu2);
+        tvResultado = findViewById(R.id.lblResultadoMulti);
+    }
+    public void CalcularMulti(View v){
+        double num1 = Double.parseDouble(edNumero1.getText().toString());
+        double num2 = Double.parseDouble(edNumero2.getText().toString());
+        double resultado = num1 * num2;
+        tvResultado.setText(resultado+"");
     }
 }
